27cc57581df965a86cecea92b1c8bdd473b32b75
scherbaks <40296886+scherbaks@users.noreply.github.com>
2018-06-27 14:25:24 +0300

    Add files via upload

--- MyPingPongProgect/Pong/src/main/java/model/logic/PongReceiver.java
+++ MyPingPongProgect/Pong/src/main/java/model/logic/PongReceiver.java
@@ -9,24 +9,22 @@ import java.io.Serializable;
 
 public class PongReceiver implements MessageListener, Serializable {
     public PongReceiver() {
-        }
+                }
 
     public void onMessage(Message message) {
         ObjectMessage my_message = (ObjectMessage)message;
 
         try{
-            Thread receiver = Thread.currentThread();
-            System.out.println("\n" + receiver.getId()+ "\n");
-
             MyMessage recievemessage = (MyMessage) my_message.getObject();
             System.out.println("The Pong following message in queue: " + recievemessage.getDate() + " \"Your value is: " + recievemessage.getValue() + "\"");
 
             ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:config.xml");
             PongSender sender = (PongSender) ctx.getBean("sendqueue");
             sender.sendMessage();
+            Thread receiver = Thread.currentThread();
+            System.out.println("Thread.getId=" + receiver.getId());
 
-            Thread.sleep(10000);
-
+            Thread.sleep(1000000000);
         } catch (JMSException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
